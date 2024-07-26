192038ea0158f814d6c7d4b31a5986f4751cc875
Youngmin Jo <54759844+designeryoungmin@users.noreply.github.com>
2024-07-27 00:10:01 +0900

    Feat: Add Streamlit Features

--- links_scraping.py
+++ links_scraping.py
@@ -1,10 +1,9 @@
+import streamlit as st
 import requests
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
-# 링크 스크래핑 할 웹 페이지의 URL
-target_url = "your_target_url"
-
+# 링크 스크래핑 함수
 def scrape_links(url):
     try:
         # 웹 페이지 요청, 타임아웃 5초 설정
@@ -17,21 +16,35 @@ def scrape_links(url):
         soup = BeautifulSoup(response.text, 'html.parser')
         
         # img 태그 내 src 속성의 링크 수집 # base_url + relative_path를 통해 절대 경로로 링크 저장
-        img_links = [urljoin(target_url, img['src']) for img in soup.find_all('img', src=True)]
+        img_links = [urljoin(url, img['src']) for img in soup.find_all('img', src=True)]
         
         # a 태그 내 href 속성의 링크 수집
-        a_links = [urljoin(target_url, a['href']) for a in soup.find_all('a', href=True)]
+        a_links = [urljoin(url, a['href']) for a in soup.find_all('a', href=True)]
         
         # set 사용하여 중복 링크 제거
         all_links = set(img_links + a_links)
         
-        # 결과 출력
-        print(f"[+] 결과 | 타겟: {target_url}") 
-        for link in all_links:
-            print(link)
+        return all_links
     except requests.exceptions.Timeout:
-        print(f"[-] Error: 연결에 문제가 없는지 확인하세요. | URL: {url}")
+        return ["Error: 연결에 문제가 없는지 확인하세요."]
     except requests.exceptions.RequestException as e:
-        print(f"[-] Error: 요청에 실패했습니다. | Error Message: {e}")
+        return [f"Error: 요청에 실패했습니다. | Error Message: {e}"]
 
-scrape_links(target_url)
+# Streamlit 애플리케이션
+st.title("Link Scraper")
+
+# 타겟 URL 입력
+target_url = st.text_input("링크 수집 할 URL을 입력하세요: ")
+
+# 링크 스크래핑 버튼
+if st.button("링크수집 시작"):
+    if target_url:
+        # 링크 스크래핑 수행
+        links = scrape_links(target_url)
+        
+        # 결과 출력
+        st.subheader("링크수집 결과")
+        for link in links:
+            st.write(link)
+    else:
+        st.error("[-] URL을 입력해주세요.")
